46c6822c6d252ad87a80e994c9c8c6a7c01df43b
Ayane Takagi <ayane1123@mailbox.unideb.hu>
2026-02-22 19:05:18 +0100

    feat(routing): set up authentication pages

--- blogging-website-frontend/src/App.jsx
+++ blogging-website-frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
+
     return (
-        <Navbar />
+        <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path="/signup" element={<hi>Sign up page</hi>} />
+                <Route path="/signin" element={<hi>Sign in page</hi>} />
+            </Route>
+        </Routes>
     )
+
 }
 
 export default App;
