d89f426649a6cff165b71538773f412ce0082068
Ayane Takagi <ayane1123@mailbox.unideb.hu>
2026-02-22 20:26:22 +0100

    feat(auth): implement user authentication form with input components

--- blogging-website-frontend/src/App.jsx
+++ blogging-website-frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
 
     return (
         <Routes>
             <Route path="/" element={<Navbar />}>
-                <Route path="/signup" element={<hi>Sign up page</hi>} />
-                <Route path="/signin" element={<hi>Sign in page</hi>} />
+                <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+                <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
             </Route>
         </Routes>
     )
