2400f250f18678b3d2d2fbe657fccab7f45d6154
Ayane Takagi <ayane1123@mailbox.unideb.hu>
2026-03-08 09:29:55 +0100

    feat(home-page): add category filters and trending blogs sidebar

--- blogging-website-frontend/src/pages/home.page.jsx
+++ blogging-website-frontend/src/pages/home.page.jsx
@@ -5,12 +5,15 @@ import axios from "axios";
 import Loader from "../components/loader.component"
 import BlogPostCard from "../components/blog-post.component";
 import MinimalBlogPost from "../components/nobanner-blog-post.component";
+import { activeTabRef } from "../components/inpage-navigation.component";
 
 const HomePage = () => {
 
     let [ blogs, setBlog ] = useState(null);
-    let [ trendingBlogs, setTrendingBlog ] = useState(null)
+    let [ trendingBlogs, setTrendingBlog ] = useState(null);
+    let [ pageState, setPageState ] = useState("home");
 
+    let categories = ["programming", "hollywood", "film making", "social media", "cooking", "tech", "finances", "travel"];
 
     const fetchLatestBlogs = () => {
         axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
@@ -32,10 +35,34 @@ const HomePage = () => {
         })
     }
 
+    const loadBlogByCategory = (e) => {
+
+        let category = e.target.innerText.toLowerCase();
+
+        setBlog(null);
+
+        if (pageState == category) {
+            setPageState("home");
+            return;
+        }
+
+        setPageState(category);
+
+    }
+
     useEffect(() => {
-        fetchLatestBlogs();
-        fetchTrendingBlogs();
-    }, [])
+
+        activeTabRef.current.click();
+
+        if (pageState == "home") {
+            fetchLatestBlogs();
+        }
+
+        if (!trendingBlogs) {
+            fetchTrendingBlogs();
+        }
+
+    }, [pageState]);
 
     return (
         <AnimationWrapper>
@@ -43,7 +70,7 @@ const HomePage = () => {
                 {/* latest blogs */}
                 <div className="w-full">
 
-                    <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]}>
+                    <InPageNavigation routes={[pageState, "trending blogs"]} defaultHidden={["trending blogs"]}>
 
                         <>
                             {
@@ -70,8 +97,37 @@ const HomePage = () => {
                 </div>
 
                 {/* filters and trending blogs */}
-                <div>
+                <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-1 border-grey pl-8 pt-3 max-md:hidden">
+                    <div className="flex flex-col gap-10">
 
+                        <div>
+                            <h1 className="font-medium text-xl mb-8">Stories from all interests</h1>
+
+                            <div className="flex gap-3 flex-wrap">
+                                {
+                                    categories.map((category, i) => {
+                                        return <button onClick={loadBlogByCategory} className={"tag " + (pageState == category ?  "bg-black text-white " : " ")} key={i}>
+                                            { category }
+                                        </button>
+                                    })
+                                }
+                            </div>
+                        </div>
+
+                        <div>
+                            <h1 className="font-medium text-xl mb-8">Trending <i className="fi fi-rr-arrow-trend-up"></i></h1>
+
+                            {
+                                trendingBlogs == null ? <Loader /> :
+                                trendingBlogs.map((blog, i) => {
+                                    return <AnimationWrapper transition={{ duration: 1, delay: i*.1 }} key={i}>
+                                        <MinimalBlogPost blog={blog} index={i} />
+                                    </AnimationWrapper>
+                                })
+                            }
+                        </div>
+
+                    </div>
                 </div>
             </section>
         </AnimationWrapper>
