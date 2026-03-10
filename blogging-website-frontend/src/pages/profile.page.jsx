fab318b998e692dfc9a4510e92957af30f45c093
Ayane Takagi <ayane1123@mailbox.unideb.hu>
2026-03-10 13:47:57 +0100

    WIP: user profile page

--- blogging-website-frontend/src/pages/profile.page.jsx
+++ blogging-website-frontend/src/pages/profile.page.jsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+export const profileDataStructure = {
+    personal_info: {
+        fullname: "",
+        username: "",
+        profile_img: "",
+        bio: "",
+    },
+    account_info: {
+        total_posts: 0,
+        total_blogs: 0,
+    },
+    social_links: { },
+    joinedAt: " "
+}
+
 const ProfilePage = () => {
+
+    let { id: profileId } = useParams();
+
+    let [ profile, setProfile ] = useState(profileDataStructure);
+
+    const fetchUserProfile = () => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-profile", { username: profileId })
+        .then(({ data: user }) => {
+            console.log(user);
+            setProfile(user);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        fetchUserProfile();
+    }, [])
+
     return (
-        <h1>Profile page from component</h1>
+        <h1>Profile page - {profileId} </h1>
     )
 }
 
