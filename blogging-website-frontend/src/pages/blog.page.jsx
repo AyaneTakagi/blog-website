b52a57afafde86654383cdb16fac4f97280ab36a
Ayane Takagi <ayane1123@mailbox.unideb.hu>
2026-03-11 09:24:17 +0100

    feat(api): add /get-blog route and increment blog/user read counts

--- blogging-website-frontend/src/pages/blog.page.jsx
+++ blogging-website-frontend/src/pages/blog.page.jsx
@@ -1,6 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const BlogPage = () => {
+
+    let { blog_id } = useParams();
+
     return (
-        <h1>This is a blog page from component JSX file</h1>
+        <h1>This is a blog page for - {blog_id}</h1>
     )
 }
 
